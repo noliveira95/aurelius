8bab93bfadb0def4e465fe08e27106a40c4c82ca
Nick <noliveira95@outlook.com>
2022-12-16 19:50:14 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='main'>
+      <h1>Aurelius</h1>
+      <div className='quote'>
+        "Memento Mori"
+      </div>
+      <button className='generate-quote'>New Quote</button>
+    </div>
+  )
+}
+
+export default App
