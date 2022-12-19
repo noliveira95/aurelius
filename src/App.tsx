2a8ca67e1cdbda79772c238dffc6c395eedfe52a
Nick <noliveira95@outlook.com>
2022-12-18 17:39:58 -0800

    feat: add author

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className='main'>
       <h1>Aurelius</h1>
+      <h2>Stoic Quote Generator</h2>
       <RandomQuote />
     </div>
   )
--- src/components/RandomQuote.tsx
+++ src/components/RandomQuote.tsx
@@ -5,10 +5,12 @@ const URL = "https://stoicquotesapi.com/v1/api/quotes/random";
 
 export default function RandomQuote() {
     const [quote, setQuote] = useState("Memento Mori");
+    const [author, setAuthor] = useState("");
 
     function getQuote() {
       axios.get(URL).then((res) => {
         setQuote(res.data.body);
+        setAuthor(res.data.author);
       }).catch((err) => {
         console.log(err);
       })
@@ -18,6 +20,9 @@ export default function RandomQuote() {
       <div className='quote-container'>
         <div className='quote'>
           "{quote}"
+        </div>
+        <div className='author'>
+          - {author}
         </div>
         <button className='generate-quote' onClick={getQuote}>New Quote</button>
       </div>
